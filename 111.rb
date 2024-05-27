f69651e894cbd7235e934079297b64e6e105f179
The Prophet 928 <Gutmane7@gmail.com>
2024-05-27 16:40:14 -0400

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines.
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+puts 1 + 982
+puts 31 - 1
+puts 3 * 4
+puts 8 / 2
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 5/0
