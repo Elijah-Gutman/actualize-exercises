6f7e7e4a82de898bb08f93dc08e0383530305bef
The Prophet 928 <Gutmane7@gmail.com>
2024-05-28 15:08:52 -0400

    Create 113.rb

---
+++ 113.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Write code to store 3 different numbers in three different variables.
+# Then write the code to print the three variables added together.
+x = "1"
+y = "2"
+z = "3"
+puts x
+puts x + y + z
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names.
+# Find the invalid variable names and delete the special characters
+# (be sure to leave the special characters in the valid variable names alone).
+
+# I deleted all of the lines from the exercise that caused a crash these were the remains :)
+bob7 = 50
+@bob = 50
+bob_ = 50
+_bob = 50
+$bob = 50
