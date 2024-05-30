b1c974ba0bb54d2d94c1bdb9b37b55b1cd5caaf5
Elijah Gutman <Gutmane7@gmail.com>
2024-05-30 19:38:31 -0400

    edits

--- 122.rb
+++ 122.rb
@@ -6,7 +6,7 @@ puts "Please type your name"
 name = gets
 puts "please enter your favorite color"
 color = gets
- puts " Your name is " + name + "and your favoirte color is  " + color
+ puts " Your name is " + name + "and your favorite color is  " + color
 
 # QUESTION 2
 # The following code has a mistake on one line that doesn't cause the program to crash,
@@ -14,6 +14,6 @@ color = gets
 # Explain where the mistake is occurring, then fix the code
 # (this may require changing more than one line).
 puts "Please enter a number:"
-number = gets.chomp.to_i
+number = gets.chomp.to_i  # Changing the gets prompt to an integer so it can be multiplied by the integer 2
 doubled_number = number * 2
-puts "The doubled number is " + doubled_number.to_s
+puts "The doubled number is " + doubled_number.to_s #changing doubled_number to a string so it can be concatenated properly
