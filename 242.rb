df5eca93309bee7542fa6b3d00218139b55cc165
Elijah Gutman <Gutmane7@gmail.com>
2024-06-24 14:59:45 -0400

    edits

---
+++ 242.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+movies = ["terminator", "star wars", "lord of the rings"]
+movies.push("the hobbit")
+
+pp movies
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+stacy = {"height" => 100, "weight" => 100}
+stacy.store("waistline", 32)
+pp stacy
