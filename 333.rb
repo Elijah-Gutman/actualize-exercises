a9cb63eb60d6d337793486def74e60db41e26b6f
Elijah Gutman <Gutmane7@gmail.com>
2024-07-11 19:38:34 -0400

    edits

---
+++ 333.rb
@@ -0,0 +1,55 @@
+# QUESTION 1
+# Create a program that asks the user to enter 5 numbers. 
+# Then, tell the user the mean average of all the numbers.
+# The mean average is defined as the sum of all the numbers 
+# divided by the count of how many numbers there are. 
+# For example, if the user enters: 11, 7, 30, 22, 55
+# the average will be 25. This is because 11 + 7 + 30 + 22 + 55 = 125, and 125 / 5 
+# (the amount of numbers in the list) = 25.
+
+# Note: If you run the code p 9 / 2, it prints 4 even though the result should be 4.5. 
+# This is because when dividing integers in Ruby, the result will be an integer. 
+
+# puts "enter 5 numbers with a comma after each"
+# average_number = gets.chomp
+# def multiple_values
+#     return 1, 2, 3, 4, 5
+# end
+# def print_values
+#     values = multiple_values
+#     _1 = values.first
+#     _2 = values[1]
+#     _3 = values[2]
+#     _4 = values[3]
+#     _5 = values[4]
+# end
+
+
+index = 0
+average_input = []
+
+while index < 5
+    puts "type a number"
+    input = gets.chomp.to_i
+    average_input.push(input)
+    puts "Array index: " + average_input.to_s
+    index = index + 1
+end
+
+
+result = 0
+for i in (average_input) do 
+  result += i  
+end
+puts result / 5
+
+# index2 = 0
+# while index2 < 5
+# average_input.each do |result|
+#   final_result = result[index2]
+#   index2 = index2 + 1
+#   puts final_result / 5
+# end
+# end
+ 
+
---
+++ 334.rb
@@ -0,0 +1,29 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Have the user enter 10 words, and allow for duplicate words. 
+# After the user is done, the program will tell the user 
+# which word was entered the most frequently.
+# For example, if the user enters: 
+#   apple, banana, orange, pear, apple, pear, apple, squash, apple, pear
+# The program will say: "apple was your most common word"
+# (That's because apple appeared in the user's list 4 times, more than any other word.)
+
+
+index = 0
+word_array = []
+
+while index < 10
+    puts "type a word"
+    input = gets.chomp
+    word_array.push(input)
+    puts "Array index: " + word_array.to_s
+    index = index + 1
+end
+
+def most_common_value(word_array)
+  word_array.max_by { |val| word_array.count(val) }
+end
+puts most_common_value(word_array) + " was your most common word"
