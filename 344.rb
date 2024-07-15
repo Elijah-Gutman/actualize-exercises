c19bb5a5ef80d618d4a9078987a04685adeff0c9
Elijah Gutman <Gutmane7@gmail.com>
2024-07-15 15:45:38 -0400

    edits

--- 344.rb
+++ 344.rb
@@ -6,6 +6,13 @@
 # Given an integer, write a method to return its roman numeral representation.
 # (1 is I, 2 is II, 3 is III, 4 is IV, 5 is V, 6 is VI, 7 is VII, etc.)
 
+# i think i did pretty well with this one but I cannot for the life of me figure out 
+# how to make it work beyond the number 9, 
+# per usual i solve the problem only using my knowledge and API's before taking
+# a look at what chat gpt might have to say
+# It made me feel quite good that there was no way to solve this in its
+# entirety with just my current knowledge at least according to chat gpt
+
 roman_numeral =
 {
   1000 => "M", 
