c59546f1290765c6ba1963182929d4436450b63e
Elijah Gutman <Gutmane7@gmail.com>
2024-07-17 19:55:26 -0400

    edits

---
+++ 412.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns its sum.
+# Then run the method and print the result.
+
+
+def sum_up(number)
+  return number.sum
+end
+
+pp sum_up([1,2,3])
