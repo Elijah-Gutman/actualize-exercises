91782b1d912a3b6a685cfaeaf06beb052cae5f86
Elijah Gutman <Gutmane7@gmail.com>
2024-07-21 12:59:21 -0400

    edits

---
+++ 422.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Write a method that takes in a string and a number n and returns the string repeated n times.
+# Then run the method and print the result.
+
+def string_multiplier(word)
+  return word * 3
+end
+
+pp string_multiplier("hello")
