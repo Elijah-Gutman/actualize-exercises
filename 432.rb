891d4ea3077d4995196364c0ff17a746c56f001b
Elijah Gutman <Gutmane7@gmail.com>
2024-07-22 19:39:08 -0400

    edits

---
+++ 432.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns the mean average.
+# Then run the method and print the result.
+# 
+#
+arr = [0,4,8,2,5,0,2,6]
+ def average(number)
+   number.sum.fdiv(number.size)
+ end
+
+pp average(arr)
+
+ 
+
+# arr.sum.fdiv(arr.size)
