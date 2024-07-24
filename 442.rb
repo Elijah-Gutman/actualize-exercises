c93bac4eda961a42a9f2ea29a805bd47d3130e4d
Elijah Gutman <Gutmane7@gmail.com>
2024-07-24 19:35:08 -0400

    edits

---
+++ 442.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns an array with every number doubled.
+# Then run the method and print the result.
+# 
+
+def double(number1, number2, number3)
+  
+  return number1 * 2, number2 * 2, number3 * 2
+end
+pp double(1, 2, 3)
