58661f1b813a71624c34b0559aef04b230c9b6bb
Elijah Gutman <Gutmane7@gmail.com>
2024-07-30 14:30:34 -0400

    edits

---
+++ 522.rb
@@ -0,0 +1,57 @@
+# QUESTION 1
+# Write a method that takes in the array of hashes below 
+# and returns a new array of strings from the model of each item. 
+# Then run the method and print the result.
+ cars = [
+  { "make" => "Toyota", "model" => "Corolla", "year" => 2018 },
+  { "make" => "Honda", "model" => "Civic", "year" => 2020 },
+  { "make" => "Ford", "model" => "Mustang", "year" => 2019 }
+ ]
+
+# new_cars = []
+# index = 0
+# while index < 1
+#   car = cars[index]
+#   new_cars << cars[0]["model"] + " " + cars[1]["model"] + " " + cars[2]["model"]
+#   index = index + 1
+# end
+# pp new_cars
+
+# new_cars = []
+# index = 0
+# while index < 0
+#   car = cars[index]
+
+#   index = index + 1
+
+#   def car_array(cars)
+#     return new_cars << cars[0]
+#   end
+# end
+
+# pp car_array()
+
+
+
+# strings = ["hello", "goodbye"]
+# new_strings = []
+# index = 0
+# while index < strings.length
+#   string = strings[index]
+#   new_strings << string.upcase
+#   index = index + 1
+# end
+# p new_strings
+# 
+def extract_models(cars)
+  cars.map { |show_models| show_models["model"] }
+end
+
+
+
+
+
+models = extract_models(cars)
+pp models
+
+
--- example.rb
+++ example.rb
@@ -1,2 +1,4 @@
-letters = %w(a aa aaa aaaa)
-pp letters
+
+a= [1,2,3,4,5,6,7,8,9,10]
+
+pp [0,4,6].map{|i| a[i]}
