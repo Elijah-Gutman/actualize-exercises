301d4b2fae255f0ddd4bf97fb490321925f33fb8
Elijah Gutman <Gutmane7@gmail.com>
2024-09-09 19:36:16 -0400

    finding lost files

---
+++ practice6.rb
@@ -0,0 +1,14 @@
+class Product 
+  attr_writer :name, :price, :metadata
+  attr_reader :name, :price, :metadata
+
+
+  def initialize (name, price, metadata)
+  @name = name
+  @price = price
+  @metadata = metadata
+  end
+end
+product = Product.new("android"," 300$" ,{ brand: "pixel4a", color: "black" })
+
+puts "Product Name: #{product.name}, Price: #{product.price}, brand: #{product.metadata[:brand]}, color: #{product.metadata[:color]}"
