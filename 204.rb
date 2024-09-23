a5453fb6f5c29f851e58cb1a489561a74c3bacf5
Elijah Gutman <gutmane7@gmail.com>
2024-09-23 19:40:18 -0400

    career inventory

--- 204.rb
+++ 204.rb
@@ -4,3 +4,5 @@
 # Fill in the answers for each question in the Career Inventory.
 # Change the sharing settings of the document to "Anyone with the link can view" 
 # Copy the link to your Career Inventory Google document and paste it below.
+
+https://docs.google.com/document/d/1m6OEM5zQaxsg0MS8xUiQ3P1ef775ZN4EZq55U3LFI0M/edit#heading=h.ywv0kijypsb
