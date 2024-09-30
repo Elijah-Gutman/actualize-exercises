894743add848e53a4ab61da588fb240cb1bef284
Elijah Gutman <gutmane7@gmail.com>
2024-09-30 16:07:16 -0400

    review

--- cohortreview.rb
+++ cohortreview.rb
@@ -56,18 +56,44 @@
 # QUESTION 1
 # Write code that prints all the years between 1980 to 2020 (including 1980 and 2020).
 
-year = 1980
-while year <=2020
-  puts year
-  year = year + 1
+# year = 1980
+# while year <=2020
+#   puts year
+#   year = year + 1
+# end
+
+
+# # QUESTION 2
+# # The following code is supposed to print the numbers 1 through 10, but it has errors.
+# # Fix the errors with the code below.
+# count = 1
+# while count <= 10
+#   puts count
+#   count = count + 1
+# end
+
+# QUESTION 1
+# Rewrite the while loop using a .times loop, then rewrite it again using a .each loop.
+numbers = [4, 3, 1, 5]
+new_numbers = []
+index = 0
+# while index < numbers.length
+#   number = numbers[index]
+#   new_numbers.push(number + 5)
+#   index = index + 1
+# end
+numbers.length.times do
+  number = numbers[index]
+  new_numbers.push(number + 5)
+  index = index + 1
 end
+pp new_numbers
 
 
 # QUESTION 2
-# The following code is supposed to print the numbers 1 through 10, but it has errors.
 # Fix the errors with the code below.
-count = 1
-while count <= 10
-  puts count
-  count = count + 1
+colors = ["red", "green", "blue"]
+colors.each do |color|
+  puts color.upcase
+  index = index + 1
 end
