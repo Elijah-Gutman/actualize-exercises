7fb40a3d2969b5be07f46f37ee84051a180f9200
Elijah Gutman <gutmane7@gmail.com>
2024-10-07 14:26:52 -0400

    review

--- cohortreview.rb
+++ cohortreview.rb
@@ -383,13 +383,36 @@
 # If the user's favorite number is between 50 and 100, give fortune Y. 
 # If the user's favorite number is above 100, give fortune Z.
 
-puts "whats is your favorite number?"
-number = gets.chomp.to_i
+# puts "whats is your favorite number?"
+# number = gets.chomp.to_i
 
-if number < 50
-  puts "You don't tyhink highly of yourself"
-elsif number > 50 && number < 100
-    puts "Of average intellect"
-else
-  puts " you think big!"
-end
+# if number < 50
+#   puts "You don't tyhink highly of yourself"
+# elsif number > 50 && number < 100
+#     puts "Of average intellect"
+# else
+#   puts " you think big!"
+# end
+
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Ancay ouyay igurefay histay neoay utoay?
+# Pig Latin is a very sophisticated language in which 
+# each English word is converted as follows: 
+# The first letter of each word is removed from the beginning and added to the end. 
+# Then, the letters 'ay' are added to the very end of the word.
+# For example: hello = ellohay, pingpong = ingpongpay, marmalade = armalademay, etc.
+# Write a program that asks the user to enter a single word 
+# and prints out the Pig Latin version of that word.
+
+puts "type out a word and have it become IPAG ATINLAY"
+word= gets.chomp
+
+pig_latin_word = word[1..9] + word[0] + "ay"
+
+puts pig_latin_word
+
+
