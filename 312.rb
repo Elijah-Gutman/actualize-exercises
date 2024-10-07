9121c68714b22237a4038336e683c6fdb1347913
Elijah Gutman <gutmane7@gmail.com>
2024-10-07 13:49:37 -0400

    review

--- 312.rb
+++ 312.rb
@@ -3,8 +3,19 @@
 
 numbers_array = [4, 53, 22, 3, 9, 7, 12]
 index = 0
+
+# for index in 0...numbers_array.length do
+#     if numbers_array[index] >= 10
+#         pp numbers_array[index]
+#         index = index + 1
+#     end
+# end
+
 while index < numbers_array.length
-    if numbers_array[index] > 10
+    if numbers_array[index] >= 10
         pp numbers_array[index]
-        index = index + 1
+    end
+    index = index + 1
 end
+
+
--- cohortreview.rb
+++ cohortreview.rb
@@ -310,51 +310,69 @@
 # pp person.height
 
 
+# # QUESTION 1
+# # Start with the Person class defined below.
+# # Add a method called email that returns the person's email address as a string. 
+# #     The person's email has the format: "firstname.lastname@gmail.com".
+# # Add a method called info that returns a string that describes the person's hobbies 
+# #     in a user-friendly format. Make sure it works no matter how many hobbies a person has! 
+# #     (You'll need to use a loop!)
+# class Person
+#   def initialize(first_name, last_name, hair_color, email, hobbies)
+#     @first_name = first_name
+#     @last_name = last_name
+#     @hair_color = hair_color
+#     @email = email
+#     @hobbies = hobbies
+#   end
+#   def info
+#     pp @hobbies
+#     result = []
+#     index = 0
+#     while index < @hobbies.length
+#       result << @hobbies[index]
+#       index = index + 1
+#     end
+#     return result.join(",")
+#   end
+# end
+
+# person = Person.new("Bob", "Jones", "pink", "Bob.Jones@gmail.com", ["basketball", "chess", "phone tag"])
+# puts "Bob Jones Enjoys the following #{person.info}"
+
+
+# # QUESTION 2
+# # The code below crashes when it is run.
+# # First explain what the error message means, 
+# # then fix the code by modifying only the line marked # FIX THIS LINE.
+# # (This one is quite tricky!)
+# class Product
+#   attr_reader :name, :price, :description
+
+#   def initialize(options_hash)
+#     @name = options_hash[:name]
+#     @price = options_hash[:price]
+#     @description = options_hash[:description]
+#   end
+# end
+
+# product = Product.new({ name: "Table", price: 500, description:"Solid oak" }) # FIX THIS LINE
+# puts "The product's name is #{product.name}"
+
+
+
 # QUESTION 1
-# Start with the Person class defined below.
-# Add a method called email that returns the person's email address as a string. 
-#     The person's email has the format: "firstname.lastname@gmail.com".
-# Add a method called info that returns a string that describes the person's hobbies 
-#     in a user-friendly format. Make sure it works no matter how many hobbies a person has! 
-#     (You'll need to use a loop!)
-class Person
-  def initialize(first_name, last_name, hair_color, email, hobbies)
-    @first_name = first_name
-    @last_name = last_name
-    @hair_color = hair_color
-    @email = email
-    @hobbies = hobbies
-  end
-  def info
-    pp @hobbies
-    result = []
-    index = 0
-    while index < @hobbies.length
-      result << @hobbies[index]
-      index = index + 1
+# Use a loop to print out only the numbers greater than 10.
+
+
+numbers_array = [4, 53, 22, 3, 9, 7, 12]
+index = 0
+
+while index < numbers_array.length
+    if numbers_array[index] >= 10
+        pp numbers_array[index]
     end
-    return result.join(",")
-  end
+    index = index + 1
 end
 
-person = Person.new("Bob", "Jones", "pink", "Bob.Jones@gmail.com", ["basketball", "chess", "phone tag"])
-puts "Bob Jones Enjoys the following #{person.info}"
 
-
-# QUESTION 2
-# The code below crashes when it is run.
-# First explain what the error message means, 
-# then fix the code by modifying only the line marked # FIX THIS LINE.
-# (This one is quite tricky!)
-class Product
-  attr_reader :name, :price, :description
-
-  def initialize(options_hash)
-    @name = options_hash[:name]
-    @price = options_hash[:price]
-    @description = options_hash[:description]
-  end
-end
-
-product = Product.new({ name: "Table", price: 500, description:"Solid oak" }) # FIX THIS LINE
-puts "The product's name is #{product.name}"
