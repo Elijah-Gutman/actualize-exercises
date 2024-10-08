b33972536e185e54db4b9983c95398dae103f741
Elijah Gutman <gutmane7@gmail.com>
2024-10-08 15:45:46 -0400

    review

--- cohortreview.rb
+++ cohortreview.rb
@@ -537,16 +537,45 @@
 
 # QUESTION 1
 # Use a loop to create a new array with only the strings with 6 or fewer letters.
-words = ["correct", "horse", "battery", "staple"]
+# words = ["correct", "horse", "battery", "staple"]
 
+# index = 0
+# six_letter_words =[]
+
+# while index < words.length
+#   if words[index].length <= 5
+#     six_letter_words << words[index]
+#   end
+#   index = index + 1
+# end
+# p six_letter_words
+
+# QUESTION 1
+# Create a program that asks the user to enter any word.
+# Then print out the amount of points the word is worth in the game of Scrabble.
+# You can start with the hash below to determine how many points each letter is worth.
+# (As an example, the word "bad" is worth 3 + 1 + 2 = 6 points.)
+letter_points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 
+'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3,
+ 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1,
+  'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 
+'y'=> 4, 'z'=> 10}
+puts " type out a word... dummy"
+user_input = gets.chomp.downcase
+
+letters = user_input.split(//)
+
+scrabble_points = []
 index = 0
-six_letter_words =[]
-
-while index < words.length
-  if words[index].length <= 5
-    six_letter_words << words[index]
-  end
+while index < letters.length
+  letter = letters[index]
+  scrabble_points.push(letter_points[letter])
   index = index + 1
 end
-p six_letter_words
+pp scrabble_points
+pp scrabble_points.sum
 
+# while index < user_input.length
+#   pp letter_points[letters]
+#   index = index + 1
+# end
