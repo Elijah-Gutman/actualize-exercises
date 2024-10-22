5883ca7c7c96e9b9d72d77ba7df49a71edcd6f04
Elijah Gutman <gutmane7@gmail.com>
2024-10-22 11:19:41 -0400

    edits

---
+++ practicejs11.js
@@ -0,0 +1,18 @@
+var numbers = [1, 8, 3, 10];
+var max_sum = numbers[1] + numbers[2];
+var index1 = 0
+while (index1 < numbers.length ){
+    var index2 = 0;
+    while(index2 < numbers.length){
+        if(index1 != index2){
+            var sum = numbers[index1] + numbers[index2];
+            if(sum > max_sum){
+               var max_sum = sum ;
+            }
+        }
+        index2 = index2 +1
+    }
+    index1 = index1 +1
+
+}
+console.log(max_sum)
